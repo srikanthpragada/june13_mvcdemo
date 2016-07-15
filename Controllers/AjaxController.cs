c0971fb2951335512ed57275f7094e1aa34bca29
srikanthpragada <srikanthpragada@gmail.com>
2016-07-15 09:01:20 +0530

    15-July

---
+++ Controllers/AjaxController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class AjaxController : Controller
+    {
+        List<String> names = new List<String>()
+            {  "Anders",
+               "Scott Guthrie",
+               "Joe Stagner",
+               "Bob Tabour",
+               "Scott Allen"
+            };
+
+
+        // GET: Ajax
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public String Now()
+        {
+            return DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss");
+        }
+
+        [HttpPost]
+        public ActionResult Names(String pattern)
+        {
+            var selnames = names.Where(n => n.ToUpper().Contains(pattern.ToUpper()));
+            return PartialView("Names", selnames);
+        }
+    }
+}
