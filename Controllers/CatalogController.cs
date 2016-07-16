5a2af7ebf0b16f7935962a80f273b5895c51e026
srikanthpragada <srikanthpragada@gmail.com>
2016-07-16 10:40:24 +0530

    16-July

---
+++ Controllers/CatalogController.cs
@@ -0,0 +1,21 @@
+﻿using mvcdemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace mvcdemo.Controllers
+{
+    public class CatalogController : ApiController
+    {
+
+        public  List<Product> Get()
+        {
+            return Products.GetProducts();
+        }
+
+
+    }
+}
---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace mvcdemo.Controllers
+{
+
+    public class MessagesController : ApiController
+    {
+        private string[] messages =
+            {"Failing to plan is planning to fail",
+             "Winners never quit, quitters never win",
+             "What you give is what you get"};
+
+        public List<string> get()
+        {
+            return messages.ToList<string>();
+        }
+        public String get(int id)
+        {
+            return messages[id - 1];
+        }
+
+    }
+}
